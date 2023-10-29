24a7d3d1453c247dcb94927400fbaf6a0b40de5a
Akash Chandra Sahu <97841784+Akash23678@users.noreply.github.com>
2023-10-29 06:49:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
+
 //Getting Current Location and Weather data
-async function getDataOfCurrentLocation(latitude, longitude){
+async function getDataOfCurrentLocation(latitude, longitude) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=aa40ba8fe3484d62b88170853232210&q=${latitude},${longitude}&aqi=yes`);
     const resp_data = await response.json();
     console.log(resp_data);
     return resp_data;
 }
 //success callback function of navigator
-async function successFetch(resp_location){
+async function successFetch(resp_location) {
     console.log(resp_location);
 
     const lat = resp_location.coords.latitude;
@@ -18,11 +19,11 @@ async function successFetch(resp_location){
     printWeather(data);
 }
 //failure callBack of Navigator 
-function failToFetch(){
+function failToFetch() {
     console.log("failed To Fetch Your Current Location")
 }
 //when window load we fetch our current location 
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     const location = navigator.geolocation.getCurrentPosition(successFetch, failToFetch);
 })
 
@@ -32,31 +33,42 @@ const input = document.getElementById("city-name");
 const btn = document.getElementById("clickBtn");
 
 
-async function getData(city){
+async function getData(city) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=aa40ba8fe3484d62b88170853232210&q=${city}&aqi=yes`);
     const resp_data = await response.json();
     console.log(resp_data);
     return resp_data;
 }
 
-btn.addEventListener("click", async ()=>{
-    try{
+btn.addEventListener("click", async () => {
+    try {
         const city = input.value;
         const data = await getData(city);
         printWeather(data);
         //clearing input field after search
-        input.value=null;  
-    }catch{
+        input.value = null;
+    } catch {
         console.error("Error fetching weather data:", error);
     }
 });
-function printWeather(response){
+function printWeather(response) {
     document.getElementById("city").innerText = "City-Name: " + response.location.name;
+    //Also changing the heading name
+    let heading = document.querySelector("h3");
+    heading.innerHTML = `Weather Information Of <span style="
+    color: blue; 
+    padding: 1px 15px;
+    display: inline-block;
+    border: 2px dashed #5c2718;
+    border-radius: 25px;">
+    ${response.location.name}</span><hr>`;
+    
     document.getElementById("region").innerText = "Region: " + response.location.region;
     document.getElementById("time").innerText = "local-Time: " + response.location.localtime;
-    document.getElementById("temp").innerText = "Temperature: " + response.current.temp_c +"℃";
+    document.getElementById("temp").innerText = "Temperature: " + response.current.temp_c + "℃";
     document.getElementById("humidity").innerText = "humidity: " + response.current.humidity;
     document.getElementById("weather-cond").innerText = "Weather: " + response.current.condition.text;
     document.getElementById("wind").innerText = "wind-Speed: " + response.current.wind_kph;
     
 }
+
